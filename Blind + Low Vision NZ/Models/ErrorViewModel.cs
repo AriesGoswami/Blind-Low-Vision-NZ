cb9a8fd3b2c59773d520903d56704e5962c31383
Aries Goswami <61667010+AriesGoswami@users.noreply.github.com>
2020-10-29 20:30:06 +1300

    Add project files.

---
+++ Blind + Low Vision NZ/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Blind___Low_Vision_NZ.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
